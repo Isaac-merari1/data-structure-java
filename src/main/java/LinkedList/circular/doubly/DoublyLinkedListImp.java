5b792d83b2430865e7e8e492644be3820d5306a7
Isaac-merari1 <aduisaacmerari@gmail.com>
2023-02-27 07:53:05 +0000

    leet code task with stack

--- src/main/java/LinkedList/circular/doubly/DoublyLinkedListImp.java
+++ src/main/java/LinkedList/circular/doubly/DoublyLinkedListImp.java
@@ -2,6 +2,7 @@ package LinkedList.circular.doubly;
 
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class DoublyLinkedListImp {
--- src/main/java/Stack/Main.java
+++ src/main/java/Stack/Main.java
@@ -1,7 +1,62 @@
 package Stack;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Stack;
+import java.util.function.*;
+
 public class Main {
     public static void main(String[] args){
+
+        String str = "()()(((())))" ;// "()()((((()))))"; // (())((()))(())((((()))))
+        int depth =1;
+        int maxDepth = 0;
+        Stack<String> stack = new Stack<>();
+
+        for(int i = 0; i< str.length(); i++){
+            String value = Character.toString(str.charAt(i));
+            if(value.equals("(")){
+                if(!stack.isEmpty()){
+                    depth +=1;
+                }
+                if(depth > maxDepth){
+                    maxDepth = depth;
+                }
+                stack.push(value);
+            } else if(value.equals(")")){
+                stack.pop();
+                if(stack.isEmpty()){
+                    depth =1;
+                } else{
+                    depth -=1;
+                }
+            }
+        }
+
+        System.out.println( "depth -> " + maxDepth);
+
+
+
+
+
+
+        /*
+        Comparable(old) & Comparator -> both for comparisons
+        Comparable -> interface
+        Comparator -> functional interface : use it as a lambda function
+        functional interface has only one abstract method
+        () -> {}
+
+         */
+//        Comparator
+      /*  Function<Integer, Integer> f = v->v;
+        Supplier<String> s = () -> "hey";
+        Consumer<String> c = v -> System.out.println(v);
+        Predicate<String> p = v -> true;
+        List<String> test = new ArrayList<>()
+        test.stream().
+        */
 
     }
 }
